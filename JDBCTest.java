86e40c76f6c0217191a9a637e62911090097a734
jarturomora <jarturomora@gmail.com>
2016-12-01 14:52:06 -0600

    JDBC for MySQL test added

---
+++ JDBCTest.java
@@ -0,0 +1,21 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+ 
+class JDBCTest {
+ 
+    private static final String url = "jdbc:mysql://localhost";
+ 
+    private static final String user = "root";
+ 
+    private static final String password = "mj110403";
+ 
+    public static void main(String args[]) {
+        try {
+            Connection con = DriverManager.getConnection(url, user, password);
+            System.out.println("Success");
+ 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
